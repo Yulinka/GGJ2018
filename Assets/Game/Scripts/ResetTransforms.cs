04393e2f5ac641bc8f2c07a28e53611f322a806d
Jason Spafford <nullprogrammer@gmail.com>
2018-01-27 00:41:15 -0800

    Brought billboard into jason scene

---
+++ Assets/Game/Scripts/ResetTransforms.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ResetTransforms : MonoBehaviour
+{
+    public bool ResetPosition = false;
+    public bool ResetScale = false;
+    public bool ResetRotation = false;
+
+    void Start()
+    {
+        enabled = Application.isEditor;
+    }
+
+    void Update()
+    {
+        if (ResetPosition)
+            transform.localPosition = Vector3.zero;
+        if (ResetRotation)
+            transform.localRotation = Quaternion.identity;
+        if (ResetScale)
+            transform.localScale = Vector3.one;
+    }
+}
